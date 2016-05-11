13c71b2ae9bcb6a876aa1e1bb99b235f011822db
kuschti <kuschti@gmail.com>
2016-05-11 08:54:26 +0200

    update responsive images config

--- site/config/config.php
+++ site/config/config.php
@@ -29,3 +29,23 @@ c::set('routes', array(
 ));
 
 c::set('thumbs.driver', 'im');
+
+// config for responsive images
+c::set('responsiveimages.sources', array(
+  's'  => array('width' => 320),
+  'm' => array('width' => 640),
+  'l' => array('width' => 768),
+  'xl'  => array('width' => 880),
+  'xxl'  => array('width' => 1760),
+));
+
+c::set('responsiveimages.sizes', array(
+  array(
+    'size_value' => '55em',
+    'mq_value'   => '55em',
+    'mq_name'    => 'min-width'
+  ),
+  array(
+    'size_value' => '100vw'
+  ),
+));
