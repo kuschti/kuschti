0ef8f4be5254f27ecd55cadd79c156f4077da10a
kuschti <kuschti@gmail.com>
2016-05-11 08:55:01 +0200

    update header
    - dns prefetch
    - meta tags
    - canonical

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,12 +3,24 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
 
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
+
+  <!-- Prefetch DNS for external assets (Twitter widgets etc). -->
+  <link rel="dns-prefetch" href="//www.google-analytics.com">
+  <link rel="dns-prefetch" href="//<?php echo c::get('disqus_name') ?>.disqus.com">
+
+
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+  <meta name="publisher" content="<?php echo html($site->author()) ?>" />
+  <meta name="copyright" content="<?php echo html($site->author()) ?>" />
+  <meta name="robots" content="index,follow" />
 
+  <link rel="canonical" href="<?php echo html($page->url()) ?>" />
   <link rel="alternate" type="application/rss+xml" href="https://kuschti.ch/feed" title="Blog Feed">
+  
   <?php echo css('assets/css/main.css') ?>
 </head>
 <body>
